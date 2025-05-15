78b25f2bf00271b86770909594248c03c90784ab
DevvratSahu07 <130061234+DevvratSahu07@users.noreply.github.com>
2025-05-15 13:24:33 +0530

    vite config file change

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'build', // Change this to your actual build output directory
+  },
 })
